e83a920f9e1f17331a0921e92cf75dfd12df9e05
HSB0y <st_invisibility@outlook.com>
2024-12-23 11:14:22 +0800

    Use Git LFS for large files

---
+++ lib/gcc/aarch64-linux-gnu/7.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-linaro-7.5-2019.12/configure --prefix=/home/username/Documents/gcc-linaro/gcc-install --target=aarch64-linux-gnu --enable-languages=c,c++ --disable-multilib --disable-nls";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
